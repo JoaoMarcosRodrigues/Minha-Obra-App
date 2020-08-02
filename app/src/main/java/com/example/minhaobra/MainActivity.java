6844b5a1b95d392b18e5e2ad9978d140ba59c370
João Marcos <jmorodrigues@dcomp.ufs.br>
2020-08-01 22:36:41 -0300

    Adicionando MENU e criando classe 'Profissionais'

--- app/src/main/java/com/example/minhaobra/MainActivity.java
+++ app/src/main/java/com/example/minhaobra/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.minhaobra;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageMenu;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // MENU
+        drawerLayout = findViewById(R.id.drawerLayout);
+        imageMenu = findViewById(R.id.imageMenu);
+        navigationView = findViewById(R.id.navigationView);
+
+        imageMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView.setItemIconTintList(null);
+
+        navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupWithNavController(navigationView,navController);
+
+        // FIM MENU
+
     }
 }
---
+++ app/src/main/java/com/example/minhaobra/Profissional.java
@@ -0,0 +1,69 @@
+package com.example.minhaobra;
+
+import java.util.List;
+
+public class Profissional {
+    String nomeCompleto;
+    String cpf;
+    String telefone;
+    String email;
+    List<String> areas;
+    String especialidade;
+    String descricao;
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getAreas() {
+        return areas;
+    }
+
+    public void setAreas(List<String> areas) {
+        this.areas = areas;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
